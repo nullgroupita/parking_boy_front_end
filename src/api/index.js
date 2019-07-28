cc05ce69fdf0eb23524db64377702ed9786013e4
1134923982 <1134923982@qq.com>
2019-07-28 16:50:50 +0800

    [kloe an][feat][add the login page and config the axios interceptor]

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,49 @@
 import axios from 'axios'
+import cookies from 'vue-cookies'
 
-axios.defaults.baseURL = 'http://18.179.142.236:9000'
+axios.interceptors.request.use(
+  config => {
+    config.baseURL = 'http://18.179.142.236:9000'
+    config.withCredentials = true
+    config.timeout = 5000
+    let token = cookies.get('token')
+    if (token) {
+      config.headers = {
+        'token': token
+      }
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
 
 async function getAllOrders () {
   try {
-    let response = await axios.get('/Orders')
+    let response = await axios.get('/orders')
     return response.data || []
   } catch (e) {
     console.log(e)
   }
 }
 
+async function login (params) {
+  try {
+    const response = await axios.post('/login', params)
+    if (response.data.retCode === 200) {
+      // set token
+      cookies.set('token', response.data.data)
+      return true
+    }
+    return false
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 const api = {
-  getAllOrders
+  getAllOrders,
+  login
 }
 export default api
